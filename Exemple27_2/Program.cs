1fd282d00be310b99cf5550d14a1faaaa6e6a4ac
MaksTimoshenko <124436224+MakcTiMoshenko@users.noreply.github.com>
2023-02-22 12:22:32 +0300

    Add files via upload
    Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Решение через перевод в массив. С использованием частично функции.
    Задать вопрос по поводу использования функции и на циклах фор в задаче!!

---
+++ Exemple27_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Пробовал через массив
+
+string Number(string text){
+    System.Console.Write(text);
+    return Console.ReadLine();
+}
+string? arr = Number("Введите число: ");
+
+
+    int [] b = new int[arr!.Length];
+    // int[] b = new int[arr!.Length];
+    for( int i=0; i< arr.Length; i++)
+    {
+         b[i] = Convert.ToInt32(arr[i].ToString());
+    }
+    Console.WriteLine("[" + string.Join(", ", b) +"]");
+
+int sum = 0;
+for(int i = 0; i < arr.Length; i++){
+     sum = sum + b[i];
+ }
+
+// string? arr = Number("Введите число: ");
+System.Console.WriteLine(sum);
