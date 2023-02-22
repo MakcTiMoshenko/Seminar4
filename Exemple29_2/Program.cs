d963a8985c8df10a13ab7231ee2ca5f71e6926c0
MaksTimoshenko <124436224+MakcTiMoshenko@users.noreply.github.com>
2023-02-22 12:25:48 +0300

    Add files via upload
    Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
    ОТКОРРЕКТИРОВАЛ!! Выдод Рандомного Массива из N Элементов. При вводе числа N пользователем!

---
+++ Exemple29_2/Program.cs
@@ -0,0 +1,27 @@
+﻿int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+    }
+
+void FillArray(int[] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    // for (int i = 0; i < arr.Length; i++)
+    // {
+    // System.Console.Write(arr[i] + " ");
+    // }    
+    Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+int N = ReadInt("Введите число: ");
+int[] array = new int[N];
+FillArray(array);
+PrintArray(array);
